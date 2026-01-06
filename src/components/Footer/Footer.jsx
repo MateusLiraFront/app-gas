65982245e78fac3d98b20c2112d4105d063b877e
MateusLiraFront <mlgtifront@gmail.com>
2026-01-06 20:51:41 -0300

    teste leans

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-100 px-4 py-4 text-sm text-center">
-      © 2026 - Meu App
+      © 2026 - app gas
     </footer>
   );
 }
