62812086f83a32c6a3ee8d4c65ed1061201bdb18
MateusLiraFront <mlgtifront@gmail.com>
2026-01-08 18:32:47 -0300

    header

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,22 +2,24 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Sidebar from "../components/Sidebar/Sidebar";
-import "./DefaultLayout.css"
+import "./DefaultLayout.css";
 
 export default function DefaultLayout() {
   return (
     <div className="default-layout min-h-screen flex flex-col">
       <Header />
-     <Sidebar/>
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+
       <main className="">
         <div className="">
-     
-           <Outlet />
-
+          <Outlet />
         </div>
       </main>
-
-      <Footer />
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
