a692ed9dfd81966e55afd390e1d056f138db11f8
MateusLiraFront <mlgtifront@gmail.com>
2026-01-12 17:04:48 -0300

    pré merge | ajuste defaultLayout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className=" ">
       <h2>Bem vindo, Cláudio</h2>
       <h3>Aqui está o consumo do seu gás</h3>
-      <Percent percent={23} />;
+      <Percent percent={23} />
     </div>
   );
 }
