379e9b3bc5d5b84bc63a8518ebd08c90949f9102
Kleberson Vasconcelos <skat8kvc@hotmail.com>
2026-01-12 18:23:35 -0300

    ATT

--- src/pages/Suporte/Suporte.jsx
+++ src/pages/Suporte/Suporte.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import styles from "./Suporte.module.css";
+import styles from "./Suport.module.css";
 import { Link } from "react-router-dom";
 import logGoogle from "../../assets/Google.png";
 import logFacebook from "../../assets/Facebook.png";
 
 export default function Suporte() {
   return (
-    <div className={styles.loginContainer}>
+  <div className={styles.loginContainer}>
       <div className={styles.loginBox}>
         <h1 className={styles.title}>Recuperar Senha</h1>
         <p className={styles.subtitle}>Digite o e-mail cadastrado</p>
