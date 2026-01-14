673f59856e4a62d19c475161822147cc948afb0b
Kleberson Vasconcelos <skat8kvc@hotmail.com>
2026-01-14 20:49:37 -0300

    comentario

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
 import logGoogle from "../../assets/Google.png";
 import logFacebook from "../../assets/Facebook.png";
 
 export default function Login() {
+  const [open, setOpen] = useState(false);
+  const [tipo, setTipo] = useState("Tipo de conta ▾");
+
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginBox}>
@@ -21,12 +24,45 @@ export default function Login() {
           </button>
         </div>
 
-        <p className={styles.subtitle}>Login com Email e Senha:</p>
+        {/* <p className={styles.subtitle}>Login com Email e Senha:</p> */}
 
-        <div className={styles.displayInput}>
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
+        
+        <div className={styles.inputGroup}>
+          <input
+            type="email"
+            placeholder="E-mail"
+            className={styles.input}
+          />
+
+          <div className={styles.dropdown}>
+            <button
+              className={styles.dropbtn}
+              onClick={() => setOpen(!open)}
+            >
+              {tipo}
+            </button>
+
+            {open && (
+              <div className={styles.dropdownContent}>
+                <p onClick={() => { setTipo("Visitante ▾"); setOpen(false); }}>
+                  Visitante
+                </p>
+                <p onClick={() => { setTipo("Usuário Comum ▾"); setOpen(false); }}>
+                  Usuário Comum
+                </p>
+                <p onClick={() => { setTipo("Gestor de Cliente ▾"); setOpen(false); }}>
+                  Gestor de Cliente
+                </p>
+              </div>
+            )}
+          </div>
         </div>
+
+        <input
+          type="password"
+          placeholder="Senha"
+          className={styles.input}
+        />
 
         <Link className={styles.forgot} to="/suporte">
           Esqueceu Email ou Senha?
@@ -44,3 +80,4 @@ export default function Login() {
     </div>
   );
 }
+
