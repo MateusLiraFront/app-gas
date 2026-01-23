0fcb75d0bf92e9f6f6b06df2dba2d25ac3db54b4
MateusLiraFront <mlgtifront@gmail.com>
2026-01-23 00:23:25 -0300

    Footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,49 +1,64 @@
 import styles from "./Footer.module.css";
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Footer({ isOpen, setIsOpen, variant = "default" }) {
   const closeSidebar = () => setIsOpen(false);
   const isAdmin = variant === "admin";
   const homeRoute = isAdmin ? "/admin/home" : "/home";
 
+  const linkClass = ({ isActive }) =>
+    `${styles.link} ${isActive ? styles.active : ""}`;
+
   return (
     <footer className={styles.footer}>
       <div className={styles.displayFooter}>
-        <Link to={homeRoute} className={styles.link} onClick={closeSidebar}>
-          <i className="fa-solid fa-house text-xl"></i>
+        <NavLink
+          to={homeRoute}
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }
+          onClick={closeSidebar}
+        >
+          <i className="fa-solid fa-house"></i>
           <label className={styles.title}>Home</label>
-        </Link>
+        </NavLink>
 
         {!isAdmin && (
           <>
-            <Link
+            <NavLink
               to="/dashboard"
-              className={styles.link}
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.active : ""}`
+              }
               onClick={closeSidebar}
             >
               <i className="fa-solid fa-fire-flame-curved"></i>
               <label className={styles.title}>Consumo</label>
-            </Link>
+            </NavLink>
 
-            <Link
+            <NavLink
               to="/dashgeral"
-              className={styles.link}
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.active : ""}`
+              }
               onClick={closeSidebar}
             >
               <i className="fa-solid fa-chart-simple"></i>
-              <label className={styles.title}>info</label>
-            </Link>
+              <label className={styles.title}>Info</label>
+            </NavLink>
           </>
         )}
-        <button
-          className={`${styles.buttonConfig} ${styles.link} ${
-            isOpen ? styles.open : styles.closed
-          }`}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <i className="fa-solid fa-list-ul"></i>
-          <label className={styles.title}>Config</label>
-        </button>
+      <button
+  className={`${styles.link} ${
+    isOpen ? styles.configOpen : ""
+  } ${styles.buttonConfig}`}
+  onClick={() => setIsOpen(!isOpen)}
+>
+  <i className="fa-solid fa-list-ul"></i>
+  <label className={styles.title}>Config</label>
+</button>
+
       </div>
     </footer>
   );
